093706c53914bbfcea2f6a9ccda5d0005d66314a
gokart <sergykoretsfsp@gmail.com>
2020-04-04 20:16:03 +0300

    edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
 
   def update_db
     username = Etc.getlogin
-    usbs = Dir.entries("/media/#{username}").select {|entry| File.directory? File.join("/media/#{username}",entry) and !(entry =='.' || entry == '..' || entry == 'BCEC7AA4EC7A591A'  || entry == 'B22841072840CBD3') }
+    usbs = Dir.entries("/media/#{username}").select {|entry| File.directory? File.join("/media/#{username}", entry) and !(entry =='.' || entry == '..' || entry == 'BCEC7AA4EC7A591A'  || entry == 'B22841072840CBD3') }
     if usbs.count > 0
       if File.exist?("/media/#{username}/#{usbs.first}/backup.bak")
-        system("dropdb 'shop_web_development'")
-        system("createdb 'shop_web_development'")
-        system("pg_dump shop_web_development < /media/#{username}/#{usbs.first}/backup.bak")
+        system("dropdb shop_web_development")
+        system("createdb shop_web_development")
+        system("psql shop_web_development < /media/#{username}/#{usbs.first}/backup.bak")
         render json: {success: true}
       else
         render json: {success: false, error: 'Файлу бази даних немає на флешці'}
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
       system("pg_dump shop_web_development > /home/#{username}/Projects/backup.bak")
       if File.exist?("/home/#{username}/Projects/backup.bak") && File.ctime("/home/#{username}/Projects/backup.bak").today?
         system("cp /home/#{username}/Projects/backup.bak /media/#{username}/#{usbs.first}")
-        if File.exist?("/media/#{username}/#{usbs.first}/backup.bak")
+        if File.exist?("/media/#{username}/#{usbs.first}/backup.bak") && File.ctime("/media/#{username}/#{usbs.first}/backup.bak").today?
           render json: {success: true}
         else
           render json: {success: false, error: 'Файл незаписаний на флешку, спробуйте записати файл вручну'}
