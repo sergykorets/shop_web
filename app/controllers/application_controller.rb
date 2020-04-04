f7608c3998345521101e10d49a9aa6ac3afe4312
gokart <sergykoretsfsp@gmail.com>
2020-04-04 19:04:05 +0300

    update db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,30 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def update_db
+    username = Etc.getlogin
+    usbs = Dir.entries("/media/#{username}").select {|entry| File.directory? File.join("/media/#{username}",entry) and !(entry =='.' || entry == '..' || entry == 'BCEC7AA4EC7A591A'  || entry == 'B22841072840CBD3') }
+    if usbs.count > 0
+      if File.exist?("/media/#{username}/#{usbs.first}/backup.bak")
+        system("dropdb 'shop_web_development'")
+        system("createdb 'shop_web_development'")
+        system("pg_dump shop_web_development < /media/#{username}/#{usbs.first}/backup.bak")
+        render json: {success: true}
+      else
+        render json: {success: false, error: 'Файлу бази даних немає на флешці'}
+      end
+    else
+      render json: {success: false, error: 'Флешку не вставлено'}
+    end
   end
 
   def copy_db
-    usbs = Dir.entries('/media/vandal').select {|entry| File.directory? File.join('/media/vandal',entry) and !(entry =='.' || entry == '..') }
+    username = Etc.getlogin
+    usbs = Dir.entries("/media/#{username}").select {|entry| File.directory? File.join("/media/#{username}",entry) and !(entry =='.' || entry == '..' || entry == 'BCEC7AA4EC7A591A'  || entry == 'B22841072840CBD3') }
     if usbs.count > 0
-      system("pg_dump shop_web_development > /home/vandal/Projects/backup.bak")
-      if File.exist?("/home/vandal/Projects/backup.bak") && File.ctime("/home/vandal/Projects/backup.bak").today?
-        system("cp /home/vandal/Projects/backup.bak /media/vandal/#{usbs.first}")
-        if File.exist?("/media/vandal/#{usbs.first}/backup.bak")
+      system("pg_dump shop_web_development > /home/#{username}/Projects/backup.bak")
+      if File.exist?("/home/#{username}/Projects/backup.bak") && File.ctime("/home/#{username}/Projects/backup.bak").today?
+        system("cp /home/#{username}/Projects/backup.bak /media/#{username}/#{usbs.first}")
+        if File.exist?("/media/#{username}/#{usbs.first}/backup.bak")
           render json: {success: true}
         else
           render json: {success: false, error: 'Файл незаписаний на флешку, спробуйте записати файл вручну'}
